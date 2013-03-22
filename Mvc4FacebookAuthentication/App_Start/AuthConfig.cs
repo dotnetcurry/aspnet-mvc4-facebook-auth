b9745e2c94274f4837ad1efe0bf44b5f19861860
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2013-03-22 23:54:48 +0530

    Code Commit

---
+++ Mvc4FacebookAuthentication/App_Start/AuthConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using Mvc4FacebookAuthentication.Models;
+using System.Configuration;
+
+namespace Mvc4FacebookAuthentication
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: ConfigurationManager.AppSettings["appId"],
+                appSecret: ConfigurationManager.AppSettings["appSecret"]);
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ Mvc4FacebookAuthentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc4FacebookAuthentication.MvcApplication" Language="C#" %>
